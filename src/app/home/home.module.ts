0bd6570ee789e2a24d1906af1baf6a3d00dddd02
TaisSocias <taissociasaraneda@gmail.com>
2023-09-02 10:44:42 -0400

    App final
    App final con todas sus modificaciones

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -12,7 +16,12 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   declarations: [HomePage]
 })
