4d37b75fa3285ae8a03f75741461481e8e84e10e
MatteoArMed <mate.araneda@duocuc.cl>
2023-09-21 21:31:00 -0300

    Se agrega validacion para verificar dominio de duoc cuando se recupera la contraseña

--- src/app/recuperar-contrasenna/recuperar-contrasenna.page.ts
+++ src/app/recuperar-contrasenna/recuperar-contrasenna.page.ts
@@ -14,26 +14,40 @@ export class RecuperarContrasennaPage implements OnInit {
   ngOnInit() {
   }
 
+  // Se declara la variable correo para que ngModel la reconozca desde HTML
   correoElectronico: string = '';
 
+  
   async CambioVentana(){
     this.router.navigate(['/login'])
   }
+  
+  // Funcion para verificar que el correo ingresado sea del dominio de duocuc
+  async validarCorreo(){
+    // Aqui se valida que el texto ingresado contenga los caracteres especificos requeridos
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@duocuc\.cl$/;
+    
+    // Aqui se valida que el correo cumpla con lo establecido devolviendo un valor boolean
+    function verificarCorreo(correoElectronico: string): boolean {
+      return emailRegex.test(correoElectronico);
+    }
 
-  async mostrarMensajeError() {
-    if (this.correoElectronico.trim() === ''){
-      const toast = await this.toastController.create({
-        message: 'Credenciales incorrectas. Por favor inténtalo nuevamente.',
-        duration: 2000
-      });
-      await toast.present();
-    } else {
+    // Aqui es donde se realiza la validacion del correo electronico
+    const correoValido = verificarCorreo(this.correoElectronico);
+    if (correoValido){
       this.CambioVentana()
       const toast = await this.toastController.create({
         message: 'Se ha enviado un correo con la nueva contraseña',
         duration: 2000
       });
       await toast.present();
+    } else {
+      const toast = await this.toastController.create({
+        message: 'Correo inválido. Por favor inténtalo nuevamente.',
+        duration: 2000
+      });
+      await toast.present();
     }
   }
+
 }
