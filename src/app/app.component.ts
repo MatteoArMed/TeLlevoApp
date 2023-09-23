0cde82d118f90f5952c7b6c0015b963f9a28894f
MatteoArMed <mate.araneda@duocuc.cl>
2023-09-23 18:01:17 -0300

    Actualizando las validaciones de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,80 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 import { LoginPage } from './login/login.page';
+import { MetodosFuncionesService } from './componentes/metodos-funciones.service';
+import { HomePage } from './home/home.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+
+
 export class AppComponent {
-  constructor(private toastController: ToastController) {}
+  constructor(private toastController: ToastController, private router: Router,) {}
   
   componente = LoginPage;
+  componenteHome = HomePage;
+  
+    // Lista de usuarios válidos y sus contraseñas
+    usuariosValidos = [
+      { usuario: 'data@duocuc.cl', contraseña: '1234' },
+      { usuario: 'Tais', contraseña: '1234' },
+      { usuario: 'Matteo', contraseña: '1234'},
+    ];
+    
+
+
+    validarCredenciales(Data: string, Data1: string): boolean {
+      // Buscar si las credenciales coinciden con la lista de usuarios válidos
+      const usuarioValido = this.usuariosValidos.find(usuario => {
+        return usuario.usuario === Data && usuario.contraseña === Data1;
+      });
+    
+      // Si se encontró un usuario válido, retornar true; de lo contrario, false.
+      return !!usuarioValido;
+    }
+
+    async login(usuarioIngresado: string,contraseñaIngresada: string) {
+
+      // Validar las credenciales
+      const credencialesValidas = this.validarCredenciales(usuarioIngresado, contraseñaIngresada);
+  
+      if (credencialesValidas) {
+        const toast = await this.toastController.create({
+          message: 'Credenciales válidas. Bienvenido de vuelta.',
+          duration: 2000
+        });
+        this.router.navigate(['/login'], { queryParams: { username: usuarioIngresado } });
+        await toast.present();
+      } else {
+        const toast = await this.toastController.create({
+          message: 'Credenciales Inválidas. Por favor inténtalo nuevamente.',
+          duration: 2000
+        });
+        await toast.present();      }
+    }
 
     // Funcion para verificar que el correo ingresado sea del dominio de duocuc
     async validarCorreo(Data: string){
       // Aqui se valida que el texto ingresado contenga los caracteres especificos requeridos
       const emailRegex = /^[a-zA-Z0-9._%+-]+@duocuc\.cl$/;
       
+
       // Aqui se valida que el correo cumpla con lo establecido devolviendo un valor boolean
       function verificarCorreo(Data: string): boolean {
         return emailRegex.test(Data);
       }
-          // Aqui es donde se realiza la validacion del correo electronico
+
+      // Aqui es donde se realiza la validacion del correo electronico
       const correoValido = verificarCorreo(Data);
       if (correoValido){
         const toast = await this.toastController.create({
-          message: 'Correo válido, puedes entrar.',
+          message: 'Correo válido.',
           duration: 2000
         });
         await toast.present();
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,79 +11,33 @@ import { HomePage } from '../home/home.page';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  constructor(private router: Router, private componente: AppComponent,private toastController: ToastController,) { }
 
   homePage = HomePage;
 
 
-  username: string = ''
-  password: string = ''
+  username: string = '';
+  password: string = '';
 
   usuariosValidos = [
     { usuario: 'duoc@duocuc.cl', contraseña: '1234' },
     { usuario: 'Tais', contraseña: '1234' },
   ];
 
-  constructor(private router: Router, private componente: AppComponent,private toastController: ToastController,) { }
-
   ngOnInit() {
   }
   
 
-  
-  
-  
-  // Funcion para verificar que el correo ingresado sea del dominio de duocuc
-  async validarCorreo(){
-    // Aqui se valida que el texto ingresado contenga los caracteres especificos requeridos
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@duocuc\.cl$/;
-    
-    // Obtener los valores ingresados por el usuario
-    const usuarioIngresado = this.username;
-    const contraseñaIngresada = this.password;
-  
-        // Validar las credenciales
-        const credencialesValidas = this.validarCredenciales(usuarioIngresado, contraseñaIngresada);
+  async FunciondeValidacion(){
 
-        if (credencialesValidas) {
-          this.router.navigate(['/home'], { queryParams: { username: usuarioIngresado } });
-        } else {
-          this.mostrarMensajeError;
-        }
-      // Aqui se valida que el correo cumpla con lo establecido devolviendo un valor boolean
-      // function verificarCorreo(usuarioIngresado:any): boolean {
-      //   return emailRegex.test(usuarioIngresado);
-      // }
-          // Aqui es donde se realiza la validacion del correo electronico
-      // const correoValido = verificarCorreo(this.username);
-      // if (correoValido){
-      //   const toast = await this.toastController.create({
-      //     message: 'Correo válido, puedes entrar.',
-      //     duration: 2000
-      //   });
-      //   await toast.present();
-      // } else {
-      //   const toast = await this.toastController.create({
-      //     message: 'Correo inválido. Por favor inténtalo nuevamente.',
-      //     duration: 2000
-      //   });
-      //   await toast.present();
-      // }
-    }
+    const username = this.username; 
+    const contraseña = this.password;
 
-    validarCredenciales(usuarioIngresado: string, contraseñaIngresada: string): boolean {
-      // Buscar si las credenciales coinciden con la lista de usuarios válidos
-      const usuarioValido = this.usuariosValidos.find(usuario => {
-        return usuario.usuario === usuarioIngresado && usuario.contraseña === contraseñaIngresada;
-      });
-      // Si se encontró un usuario válido, retornar true; de lo contrario, false.
-      return !!usuarioValido;
-    }
-    
-    async mostrarMensajeError() {
-      const toast = await this.toastController.create({
-        message: 'Credenciales incorrectas. Inténtalo nuevamente.',
-        duration: 2000
-      });
-      await toast.present();
-    }
+    this.componente.login(username,contraseña);
+  }
 }
+
+
+  
+  
